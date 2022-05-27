dd69e2b1bbf9b4ba6c227382aa1510fe6b10dc1e
Hattsu <102260446+Hattsu@users.noreply.github.com>
2022-05-26 22:09:02 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ByronCalderon3A_Ejercicio10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factorial factorial = new Factorial();
+            factorial.Proceso_factorial();
+        }
+    }
+}
